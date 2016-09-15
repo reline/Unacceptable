372f7e94f222eff7bbff2ca813008d9cdca1ce58
Nathan Reline <nathanielreline@gmail.com>
2016-09-15 16:26:11 -0700

    Version update

--- app/src/main/java/com/github/reline/unacceptable/MainActivity.java
+++ app/src/main/java/com/github/reline/unacceptable/MainActivity.java
@@ -8,6 +8,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -28,41 +29,36 @@ public class MainActivity extends Activity {
         // set application to control media volume instead of ring volume
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-        lemon = getDrawable(R.drawable.lemon);
-        lemongrab = getDrawable(R.drawable.lemongrab);
-
-        // setup media player
+        lemon = ContextCompat.getDrawable(this, R.drawable.lemon);
+        lemongrab = ContextCompat.getDrawable(this, R.drawable.lemongrab);
+        final Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake);
+        final Vibrator vibrator = (Vibrator) MainActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
         mediaPlayer = MediaPlayer.create(this, R.raw.lemon_grab_unacceptable);
+        final int duration = mediaPlayer.getDuration();
 
         // setup button
         unacceptableButton = (ImageView) findViewById(R.id.lemon_button);
-        assert unacceptableButton != null;
         unacceptableButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // change to lemongrab image
                 unacceptableButton.setImageDrawable(lemongrab);
-
-                // play media player
-                mediaPlayer.start();
-
-                // shake lemon
-                Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake);
-                unacceptableButton.startAnimation(shake);
-
-                // vibrate phone
-                Vibrator vibrator = (Vibrator) MainActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
-                // Vibrate for 2 seconds
-                vibrator.vibrate(2000);
-
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
+                new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         // change image back to normal
                         unacceptableButton.setImageDrawable(lemon);
                     }
-                }, 2000);
+                }, duration);
+
+                // play media player
+                mediaPlayer.start();
+
+                // shake lemon
+                unacceptableButton.startAnimation(shake);
+
+                // vibrate phone
+                vibrator.vibrate(duration);
             }
         });
     }

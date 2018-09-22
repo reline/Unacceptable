7fb724d5b96e832d895e0afd1d03469e65a91095
Nathan Reline <nathanielreline@gmail.com>
2018-09-22 16:57:07 -0700

    Stop media when app closes

---
+++ app/src/main/java/com/github/reline/unacceptable/Extensions.kt
@@ -0,0 +1,19 @@
+package com.github.reline.unacceptable
+
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.SparseArray
+
+fun <T> SparseArray<T>.getOrPut(key: Int, defaultValue: T): T {
+    return this.get(key)
+            ?: defaultValue.also { default -> this.put(key, default) }
+}
+
+fun Vibrator.vibrate(milliseconds: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        this.vibrate(VibrationEffect.createOneShot(milliseconds.toLong(), VibrationEffect.DEFAULT_AMPLITUDE))
+    } else {
+        this.vibrate(milliseconds.toLong())
+    }
+}
--- app/src/main/java/com/github/reline/unacceptable/MainActivity.kt
+++ app/src/main/java/com/github/reline/unacceptable/MainActivity.kt
@@ -4,14 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.media.AudioManager
 import android.media.MediaPlayer
-import android.os.Build
 import android.os.Bundle
-import android.os.VibrationEffect
 import android.os.Vibrator
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.unacceptable_layout.*
 
 class MainActivity : Activity() {
 
@@ -21,7 +19,7 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.unacceptable_layout)
 
         // set application to control media volume instead of ring volume
         volumeControlStream = AudioManager.STREAM_MUSIC
@@ -31,7 +29,9 @@ class MainActivity : Activity() {
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
         shake.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationStart(animation: Animation) {
+                unacceptableButton.setImageResource(R.drawable.lemongrab)
+            }
 
             override fun onAnimationEnd(animation: Animation) {
                 unacceptableButton.setImageResource(R.drawable.lemon)
@@ -47,34 +47,40 @@ class MainActivity : Activity() {
             return
         }
         shake.restrictDuration(mediaPlayer.duration.toLong())
-        mediaPlayer.start()
         unacceptableButton.startAnimation(shake)
-        unacceptableButton.setImageResource(R.drawable.lemongrab)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            vibrator.vibrate(VibrationEffect.createOneShot(mediaPlayer.duration.toLong(), VibrationEffect.DEFAULT_AMPLITUDE))
-        } else {
-            vibrator.vibrate(mediaPlayer.duration.toLong())
-        }
+        vibrator.vibrate(mediaPlayer.duration)
+        mediaPlayer.start()
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        val timeRemaining = savedInstanceState?.getInt(TIME_REMAINING)?.toLong() ?: 0
-        if (timeRemaining > 0) {
-            unacceptableButton.setImageResource(R.drawable.lemongrab)
-            shake.restrictDuration(timeRemaining)
+        val timeRemaining = savedInstanceState?.getInt(TIME_REMAINING) ?: 0
+        val isChangingConfigurations = savedInstanceState?.getBoolean(CHANGING_CONFIGURATIONS) ?: false
+        if (timeRemaining > 0 && isChangingConfigurations) {
+            shake.restrictDuration(timeRemaining.toLong())
             unacceptableButton.startAnimation(shake)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
-        val timeRemaining = if (mediaPlayer.isPlaying) mediaPlayer.duration - mediaPlayer.currentPosition else 0
-        outState?.putInt(TIME_REMAINING, timeRemaining)
+        if (mediaPlayer.isPlaying) {
+            if (isChangingConfigurations) {
+                val timeRemaining = mediaPlayer.duration - mediaPlayer.currentPosition
+                outState?.putInt(TIME_REMAINING, timeRemaining)
+                outState?.putBoolean(CHANGING_CONFIGURATIONS, true)
+            } else {
+                mediaPlayer.pause()
+                mediaPlayer.seekTo(0)
+                shake.cancel()
+                unacceptableButton.animation = null
+            }
+        }
         super.onSaveInstanceState(outState)
     }
 
     companion object {
         private const val TIME_REMAINING = "TIME_REMAINING"
+        private const val CHANGING_CONFIGURATIONS = "CHANGING_CONFIGURATIONS"
     }
 
 }
